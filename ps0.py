cecfc305a6ed551e2e9fd030bcdf7049ddd389b5
Isha09 <eshachaudhary27@gmail.com>
2017-08-09 15:46:40 -0500

    PS0 solution code

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+mit 6.0001 - PS0
+"""
+import math
+
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+print("x**y = ",x**y)
+print("log(x) =",math.log2(x))
+
